41549861484e3983fe8d4c2aab58e84ab3a0eb75
Shazud <david.pava@protonmail.com>
2021-05-01 19:23:00 +0300

    Added UserReadDto

---
+++ Dtos/UsersReadDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieUserManagerService.Read
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
