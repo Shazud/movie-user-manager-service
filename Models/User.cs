da0f891f0a811cf671195b1a739d9d08871b61e7
Shazud <david.pava@protonmail.com>
2021-05-01 18:06:19 +0300

    Added datatype specifications in User model

--- Models/User.cs
+++ Models/User.cs
@@ -5,14 +5,21 @@ namespace MovieUserManagerService.Models
     public class User
     {
         [Key]
+        [DataType(DataType.Text)]
         public string username { get; set; }
         [Required]
+        [DataType(DataType.Text)]
         public string name { get; set; }
         [Required]
+        [DataType(DataType.Text)]
         public string firstname { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string password { get; set; }
         [Required]
+        [DataType(DataType.EmailAddress)]
         public string email { get; set; }
+        [Required]
+        public bool isAdmin { get; set; }
     }
 }
