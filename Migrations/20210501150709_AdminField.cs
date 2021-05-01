3d72d8247511c527690fbf452d1243f3b9738e4d
Shazud <david.pava@protonmail.com>
2021-05-01 18:17:25 +0300

    migrations and database update for isAdmin field and basic data

---
+++ Migrations/20210501150709_AdminField.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieUserManagerService.Migrations
+{
+    public partial class AdminField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isAdmin",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAdmin",
+                table: "Users");
+        }
+    }
+}
