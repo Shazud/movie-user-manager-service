87d02389262313e85a26efc1a3012ea7e7bb54e1
Shazud <david.pava@protonmail.com>
2021-05-02 10:45:49 +0300

    Fixed Typo in UsersReadDto

--- Dtos/UsersReadDto.cs
+++ Dtos/UsersReadDto.cs
@@ -2,12 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MovieUserManagerService.Read
 {
-    public class User
+    public class UserReadDto
     {
         public string username { get; set; }
         public string lastname { get; set; }
         public string firstname { get; set; }
-        public string password { get; set; }
         public string email { get; set; }
         public bool isAdmin { get; set; }
     }
