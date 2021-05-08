f1b0840913cba671a5d4a74a2cad9cacaa00006f
Shazud <david.pava@protonmail.com>
2021-05-08 16:37:31 +0300

    Jwt Validation for DeleteUser and GetSpecificUser

--- Services/UserAuthenticationService.cs
+++ Services/UserAuthenticationService.cs
@@ -1,20 +1,25 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using MovieUserManagerService.Models;
 using MovieUserManagerService.Settings;
+using MovieUserManagerService.Utils;
 
 namespace MovieUserManagerService.Services
 {
     public class UserAuthenticationService : IUserAuthenticationService
     {
         private readonly JwtSettings _jwt;
+        private readonly JwtSecurityTokenHandler _tokenHandler;
 
         public UserAuthenticationService(JwtSettings jwt)
         {
             _jwt = jwt;
+            _tokenHandler = new JwtSecurityTokenHandler();
         }
 
         public bool ComparePasswords(string pass, string hash)
@@ -37,17 +42,51 @@ namespace MovieUserManagerService.Services
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-
-            var token = tokenHandler.CreateToken(securityTokenDescriptor);
+            var token = _tokenHandler.CreateToken(securityTokenDescriptor);
 
 
-            return tokenHandler.WriteToken(token);
+            return _tokenHandler.WriteToken(token);
         }
 
         public string HashPassword(string password)
         {
             return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 14);
+        }
+
+        public string GetTokenClaimValue(string token, string claim)
+        {
+            try
+            {
+                var val = _tokenHandler.ReadJwtToken(token).Claims.FirstOrDefault(c => c.Type == claim).Value;
+                return val;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public string GetToken(HttpContext httpContext)
+        {
+            var token =  httpContext.Request.Headers["authorization"];
+            if(token == string.Empty)
+            {
+                return string.Empty;
+            }
+            try
+            {
+                _tokenHandler.ValidateToken(token, new TokenValidationParameters{
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwt.Secret)),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+            return token;
         }
     }
 }
